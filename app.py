62532cf145e4789589c851d1594250bdd2ed35b2
Suryansh <suryanshdev999@gmail.com>
2026-04-10 08:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -632,4 +632,5 @@ def api_get_location():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
